e3efc885499acf9f43f456ea67cc9167cf2152f0
Atulp24 <apk200209@gmail.com>
2024-10-25 18:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.getElementById("removeButton").addEventListener("click", () => {
+	const colorSelected = document.getElementById("colorSelect");
+	const selectedIndex = colorSelected.selectedIndex;
+
+	if(selectedIndex !== -1) 
+	{
+		colorSelected.remove(selectedIndex);
+	}
+	else
+	{
+		alert("Please select a color to remove");
+	}
+});
